eb38835fdf7723b4161a9f606d1f9d31d9d77a5b
Matias <barcalin23@gmail.com>
2025-04-22 18:55:33 -0300

    Archivo C y preguntas respondidas

--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,13 @@
 
 int main(){
     printf ("Hola mundo");
+    int numero = 23;
+    int *puntero = &numero; //apunta a la direccion de memoria
+
+    printf("\n%d", *puntero); //muestra el contenido del puntero
+    printf("\n%p", puntero); //muestra la direccion de memoria
+    printf("\n%p", &numero); //muestra la direccion de memoria de la variable
+    printf("\n%p", &puntero); //muestra la direccion de memoria del puntero
+    printf("\n%lu", sizeof(puntero)); //muestra el tamaño del puntero
     return 0;
 }
